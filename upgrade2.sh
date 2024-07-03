34a72e3bc60069e218e625ca63d3475813ffc65c
yellow-dragon <44664703+yellow-dragon@users.noreply.github.com>
2024-07-03 22:14:56 +0700

    Update upgrade2.sh

--- upgrade2.sh
+++ upgrade2.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
+cd /app-data/miners 
+rm -f H9-Miner-spacemesh-v4.0.0-2-linux.zip && rm -rf linux
+wget https://github.com/h9-dev/spacemesh-miner/releases/download/v4.0.0/H9-Miner-spacemesh-v4.0.0-2-linux.zip 
+unzip H9-Miner-spacemesh-v4.0.0-2-linux.zip 
+rm -f killh9.sh 
+wget https://raw.githubusercontent.com/yellow-dragon/smh/main/killh9.sh 
+chmod +x killh9.sh 
+rm -f h9.sh 
+wget https://raw.githubusercontent.com/yellow-dragon/smh/main/h9.sh 
+chmod +x h9.sh
+
 
