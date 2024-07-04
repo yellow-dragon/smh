519f7bf22358f5088b6dd0869efe688cdb17fa89
yellow-dragon <44664703+yellow-dragon@users.noreply.github.com>
2024-07-04 17:53:00 +0700

    Update upgrade1.sh

--- upgrade1.sh
+++ upgrade1.sh
@@ -1,4 +1,9 @@
-#!/bin/bash
+#!/bin/bash 
+killall h9.sh
+killall h92.sh
+killall h93.sh
+killall h9-miner-spacemesh-linux-amd64 
+killall h9-miner-spacemesh-linux-cuda-amd64
 cd /app-data/miners 
 rm -f upgrade2.sh  
 wget https://raw.githubusercontent.com/yellow-dragon/smh/main/upgrade2.sh
